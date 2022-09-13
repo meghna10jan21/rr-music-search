ff1f1767a166e7e705ae9da7c4e6c0fa949b1c36
Meghna Patel <meghna10jan21@yahoo.com>
2022-09-12 21:02:31 -0500

    music-search

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import {useEffect, useState } from 'react'
+import { Fragment, useEffect, useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
-import { DataContext } from './context/DataContext'
+import AlbumView from './components/AlbumView'
+import ArtistView from './components/ArtistView'
 
 function App() {
 	let [search, setSearch] = useState('')
@@ -11,21 +13,21 @@ function App() {
 	const API_URL = 'https://itunes.apple.com/search?term='
 
 	useEffect(() => {
-		if(search) {
+		if (search) {
 			const fetchData = async () => {
 				document.title = `${search} Music`
 				const response = await fetch(API_URL + search)
 				const resData = await response.json()
 				if (resData.results.length > 0) {
-					setData(resData.results)
+					return setData(resData.results)
 				} else {
-					setMessage('Not Found')
+					return setMessage('Not Found')
 				}
 			}
 			fetchData()
 		}
 	}, [search])
-	
+
 	const handleSearch = (e, term) => {
 		e.preventDefault()
 		setSearch(term)
@@ -33,13 +35,23 @@ function App() {
 
 	return (
 		<div>
-			<SearchBar handleSearch = {handleSearch}/>
 			{message}
-			<DataContext.Provider value={data}>
-				<Gallery />
-			</DataContext.Provider>
+			<Router>
+				<Routes>
+					<Route path="/" element={
+						<Fragment>
+							<SearchBar handleSearch={handleSearch} />
+
+							<Gallery data={data} />
+						</Fragment>
+					}/>
+			<Route path="/album/:id" element={<AlbumView/>}/>
+			<Route path="/artist/:id" element={<ArtistView/>}/>		
+				</Routes>
+			</Router>
 		</div>
-  	);
+	);
 }
 
 export default App;
+
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -2,8 +2,8 @@
 import { useContext } from 'react'
 import { DataContext } from '../context/DataContext'
 import GalleryItem from './GalleryItem'
-
-function Gallery(){
+import { Link } from 'react-router-dom'
+function Gallery(props){
     const data = useContext(DataContext)
     
     const display = data.map((item,index) => {
@@ -11,11 +11,23 @@ function Gallery(){
             <GalleryItem item={item} key={index} />
         )
     })
-    return (
-        <div>
-            {display}
-        </div>
-    )
-}
-
-export default Gallery
+    const detailView = () => {
+        return (
+            <div style={detailStyle}>
+                <h2>{props.item.trackName}</h2>
+                <h3>
+                    <Link to={`/artist/${props.item.artistId}`}>
+                        {props.item.artistName}
+                    </Link>
+                </h3>
+                <h3>
+                    <Link to={`/album/${props.item.collectionId}`}>
+                        {props.item.collectionName}
+                    </Link>
+                </h3>
+                <h4>{props.item.primaryGenreName}</h4>
+                <h4>{props.item.releaseDate}</h4>
+            </div>
+        )
+    }
+    
